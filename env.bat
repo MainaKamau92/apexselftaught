f6d3908ccf44e5b134c291784cb38b06cae51f99
Maina Kamau <lewiikamaa8@gmail.com>
2019-01-01 16:38:37 +0300

    Update env.bat

--- env.bat
+++ env.bat
@@ -1,7 +1,7 @@
 @echo off
 
-set DATABASE_URL=postgresql://postgres:mainakamau@localhost/apex_db
-set SECRET_KEY='c9f130b893249c892bdc589a95cc59c8'
+set DATABASE_URL=postgresql://postgres:xxxxx@localhost/apex_db
+set SECRET_KEY='xxxxxxx'
 set FLASK_APP=run.py
 set FLASK_ENV=development
 set APP_SETTINGS=development
