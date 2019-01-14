e9a16049e54dc23e4423de867113c42617b444a2
MainaKamau92 <lewiikamaa8@gmail.com>
2019-01-15 02:38:58 +0300

    add changes to gitignore to ignore bat files

--- env.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set DATABASE_URL=postgresql://postgres:mainakamau@localhost/apex_db
-set DATABASE_TEST_URL=postgresql://postgres:mainakamau@localhost/test_db
-set APP_SETTINGS=development
-set FLASK_APP=run.py
-set SECRET_KEY='84c72df8da901d8e28c0af3f73574c73'
-set FLASK_ENV=development
