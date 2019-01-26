db032bff21f6fdbe319058cf0905dce9e772f4f1
MainaKamau92 <lewiikamaa8@gmail.com>
2019-01-26 15:17:04 +0300

    update css and index files to make site mobile responsive

--- app/auth/views.py
+++ app/auth/views.py
@@ -66,7 +66,7 @@ def login():
         user = User.query.filter_by(email=form.email.data).first()
         if user is not None and user.verify_password(form.password.data):
             login_user(user, remember=form.remember.data)
-            flash(f'Logged In', 'success')
+            #flash(f'Logged In', 'success')
             if user.is_freelancer == True and user.is_employer == False:
                 # redirect to the freelancer dashboard page after login
                 return redirect(url_for('freelancer.dashboard'))
